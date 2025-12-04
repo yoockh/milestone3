acd0f72d9508b24552a601069176e0a979a5e8cd
Aisiya Qutwatunnada <ycorp00@gmail.com>
2025-12-05 01:54:13 +0700

    refactor: simplify donation routes by removing redundant authentication group

--- be/api/routes/donation_routes.go
+++ be/api/routes/donation_routes.go
@@ -9,16 +9,10 @@ func (r *EchoRouter) RegisterDonationRoutes(donationCtrl *controller.DonationCon
 	donationRoutes := r.echo.Group("/donations")
 	donationRoutes.Use(middleware.JWTMiddleware)
 
-	// public
 	donationRoutes.GET("", donationCtrl.GetAllDonations)
 	donationRoutes.GET("/:id", donationCtrl.GetDonationByID)
-
-	// authenticated group
-	auth := donationRoutes.Group("")
-	auth.Use(middleware.JWTMiddleware)
-
-	auth.POST("", donationCtrl.CreateDonation)
-	auth.PUT("/:id", donationCtrl.UpdateDonation)
-	auth.PATCH("/:id", donationCtrl.PatchDonation)
-	auth.DELETE("/:id", donationCtrl.DeleteDonation)
+	donationRoutes.POST("", donationCtrl.CreateDonation)
+	donationRoutes.PUT("/:id", donationCtrl.UpdateDonation)
+	donationRoutes.PATCH("/:id", donationCtrl.PatchDonation)
+	donationRoutes.DELETE("/:id", donationCtrl.DeleteDonation)
 }
